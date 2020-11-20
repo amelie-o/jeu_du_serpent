e161f4870b09f5ae8c1e2240e1fcf6cc637d20f8
Amélie Ouellet <rosemimi2002@gmail.com>
2020-11-20 12:09:51 -0500

    Class Serpent commencer

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Le jeu
     class Jeu{ //Contien tout les caracteristiques de jeu
         constructor(_idSvg, _idPointage) {
-            console.log("creation du jeu");
+           // console.log("creation du jeu");
 
             this.s = Snap(_idSvg); //Récuper id de svg et le garde dans la propriété s
             this.sortiPointage = document.querySelector(_idPointage); //Récuper id de pointage et le garde dans la propriété sortiPointage
@@ -17,8 +17,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             this.finPartie();
             this.affichagePointage(1); //Détermine la longeur du serpent
             this.pomme = new Pomme(this); //Crée la pomme
-            this.serpent = new Serpent(); //Crée le Serpent
-            console.log("nouvelle Partie");
+            this.serpent = new Serpent(this); //Crée le Serpent
+           // console.log("nouvelle Partie");
         }
 
         finPartie(){
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             if(this.pomme !== undefined){ //Si la pomme à été crée
                 this.pomme.supprimePomme();//Supprime la pomme à la fin
                 this.pomme = undefined; //Reset les données de la pomme
-                console.log("Fin Partie");
+               // console.log("Fin Partie");
             }
 
         }
@@ -38,15 +38,77 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Le serpent
     class Serpent{ //Contien tout les caracteristiques de Serpent
-        constructor() {
-            console.log("creation du Serpent");
+        constructor(_leJeu) {
+            //console.log("creation du Serpent");
+            this.leJeu = _leJeu; //Référence à la class Jeu
+
+            this.currentX = -1;
+            this.currentY = 0;
+
+            this.nextMoveX = 1;
+            this.nextMoveY = 0;
+
+            this.serpentLongeur = 1;
+            this.tabCarreSerpent = [];
+
+            this.vitesse = 250;
+            this.timing = setInterval(this.controleSerpent.bind(this), this.vitesse); //Appel controleSerpent a chaque 250 ms dans le contexte du serpent
+
+            document.addEventListener("keydown", this.verifTouche.bind(this)); //Vérifi si une touche est toucher dans le contexte de Serpent
+        }
+
+        verifTouche(_evt){
+            var evt = _evt;
+            //console.log(evt.keyCode);
+
+            this.deplaceSerpent(evt.keyCode)//Dit la direction de la touche
+        }
+
+        deplaceSerpent(dirCode){
+            switch (dirCode){
+                case 37:
+                    this.nextMoveX = -1;
+                    this.nextMoveY = 0;
+                    break;
+                case 38:
+                    this.nextMoveX = 0;
+                    this.nextMoveY = -1;
+                    break;
+                case 39:
+                    this.nextMoveX = 1;
+                    this.nextMoveY = 0;
+                    break;
+                case 40:
+                    this.nextMoveX = 0;
+                    this.nextMoveY = 1;
+                    break;
+            }
+
+            //console.log(this.nextMoveX, this.nextMoveY);
+        }
+
+        controleSerpent(){
+            var nextX = this.currentX + this.nextMoveX;
+            var nextY = this.currentY + this.nextMoveY;
+
+            this.dessineCarre(nextX, nextY);
+            this.currentX = nextX;
+            this.currentY = nextY;
+        }
+
+        dessineCarre(x, y){
+
+        }
+
+        supprimeSerpent(){
+
         }
     }
 
     //La pomme
     class Pomme{ //Contien tout les caracteristiques de pomme
         constructor(_leJeu) {
-            console.log("creation de la pomme");
+            //console.log("creation de la pomme");
             this.leJeu = _leJeu; //Référence à la class Jeu
             this.tabPomme = []; //Creer un tableau
 
@@ -62,7 +124,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         supprimePomme(){
             this.tabPomme[0].remove();//Retire la pomme
-            console.log("Delete pomme");
+           // console.log("Delete pomme");
         }
     }
 
