140558f122cdb317522834c14f01921d094aebc0
Amélie Ouellet <rosemimi2002@gmail.com>
2020-11-20 10:37:04 -0500

    Creation class Jeu,Serpent et pomme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //Le jeu
+    class Jeu{ //Contien tout les caracteristiques de jeu
+        constructor() {
+            console.log("creation du jeu");
+        }
+    }
+
+    //Le serpent
+    class Serpent{ //Contien tout les caracteristiques de Serpent
+        constructor() {
+            console.log("creation du Serpent");
+        }
+    }
+
+    //La pomme
+    class Pomme{ //Contien tout les caracteristiques de pomme
+        constructor() {
+            console.log("creation de la pomme");
+        }
+    }
+
+    var unePartie = new Jeu();
 
 });
